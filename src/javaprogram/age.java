ba2d5483964177fe8dd2820734adbd39f248e815
vijay <vij842828@gmail.com>
2023-07-01 14:53:09 +0530

    java sample

---
+++ src/javaprogram/age.java
@@ -0,0 +1,28 @@
+package javaprogram;
+
+public class age {
+
+	public static void main(String[] args) {
+		
+				int age=65;
+				
+				if(18>age) {
+				System.out.println("minor");
+				}
+				else if(25>age) {
+				System.out.println("mojor");	
+				}
+				else if(45>age) {
+				System.out.println("senior");	
+				}
+				else if(65>=age) {
+				System.out.println("super senior");	
+				}
+			
+
+		}
+
+}
+	
+
+
